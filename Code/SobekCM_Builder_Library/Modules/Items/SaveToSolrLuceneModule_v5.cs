a40ada3a2cfc7972d62a7c2c246177386eb4cc18
Mark V Sullivan <Mark.V.Sullivan@gmail.com>
2026-08-08 19:27:54 +0000

    Update solr schema version

--- Code/SobekCM_Builder_Library/Modules/Items/SaveToSolrLuceneModule_v5.cs
+++ Code/SobekCM_Builder_Library/Modules/Items/SaveToSolrLuceneModule_v5.cs
@@ -26,7 +26,6 @@ namespace SobekCM.Builder_Library.Modules.Items
                 if (((!Resource.Metadata.Behaviors.IP_Restriction_Membership_Is_Null) && (Resource.Metadata.Behaviors.IP_Restriction_Membership < 0)) ||
                     ((!Resource.Metadata.Behaviors.Dark_Flag_Is_Null) && (Resource.Metadata.Behaviors.Dark_Flag)))
                 {
-
                     Solr_Controller.Delete_Resource_From_Index(Settings.Servers.Document_Solr_Index_URL, Settings.Servers.Page_Solr_Index_URL, Resource.BibID, Resource.VID);
                     return true;
                 }
@@ -37,6 +36,7 @@ namespace SobekCM.Builder_Library.Modules.Items
                 }
                 catch (Exception ee)
                 {
+                    Console.WriteLine("ERROR indexing");
                     OnError("Error saving data to the new Solr/Lucene index.  The index may not reflect the most recent data in the METS.", Resource.BibID + ":" + Resource.VID, Resource.METS_Type_String, Resource.BuilderLogId);
                     OnError("Solr Error: " + ee.Message, Resource.BibID + ":" + Resource.VID, Resource.METS_Type_String, Resource.BuilderLogId);
                     Tracer?.Add_Trace("SaveToSolrLuceneModule_v5.DoWork", "Solr Error: " + ee.Message, Custom_Trace_Type_Enum.Error);
